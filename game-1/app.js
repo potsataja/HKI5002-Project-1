e4d9d2c63185bdedf9ab3c077c400979c5c444ed
Kaido <kaido.kukk@gmail.com>
2017-06-07 00:17:42 +0300

    Lisatud võimalus lõpuks mängida, taimer töötab

--- game-1/app.js
+++ game-1/app.js
@@ -1,11 +1,98 @@
+// global variables
+var activeCards = [];
+var intervalID = "";
+var hiddenCardCount = 0;
+
 function onPageLoad() {
-    //console.log("start");
+    startGame();
+}
 
-    var cards = ['AA','X','Juku','Banaan', 'Ameerika Ühendriigid', 'Tallinna Ülikool', 'Tartu Ülikool'];
-    cards = cards.concat(cards);
-    cards.sort(function(a, b){return 0.5 - Math.random()});
 
-    for(card in cards) {
-        new Card(card).insertText(cards[card]);
+function startGame() {
+    clearGameBoard();
+
+    var cardTexts = ['AA', 'X', 'Juku', 'Banaan', 'Ameerika Ühendriigid', 'Tallinna Ülikool', 'Tartu Ülikool', 'YY'];
+    //var cardTexts = ['EMMA', 'PAUL', 'ÕNNELA', 'ÕNNE', 'MARIS', 'KAIDO'];
+
+    cardTexts = cardTexts.concat(cardTexts);
+    cardTexts.sort(function (a, b) { return 0.5 - Math.random() });
+
+    var cards = [];
+    for (cardText in cardTexts) {
+        //new Card(cardText).insertText(cardTexts[cardText])
+        var card = new Card(cardText);
+        card.insertText(cardTexts[cardText]);
+        cards.push(card);
+        hiddenCardCount++;
     }
+}
+
+function clearGameBoard() {
+    resetTimer();
+
+    activeCards = [];
+    document.getElementById("kast").innerHTML = "";
+    //var el = document.getElementById("kast");
+    //while (el.firstChild) el.removeChild(el.firstChild);  //https://jsperf.com/innerhtml-vs-removechild/418
+}
+
+function compareCards(card) {
+    if (intervalID == "") {
+        startTimer();
+    }
+
+    if (card.found || card.visible) {
+        return;
+    }
+
+    card.changeVisibility();
+
+    if (activeCards.length == 0) {
+        activeCards.push(card);
+    } else if (activeCards.length == 1) {
+        activeCards.push(card);
+
+        if (activeCards[0].text == activeCards[1].text) {
+            activeCards[0].setFound();
+            activeCards[1].setFound();
+
+            hiddenCardCount -= 2;
+
+            checkComplete();
+        }
+    } else if (activeCards.length == 2) {
+        activeCards[0].changeVisibility();
+        activeCards[1].changeVisibility();
+
+        activeCards = [];
+        activeCards.push(card);
+    }
+}
+
+function checkComplete() {
+    if (hiddenCardCount == 0) {
+        stopTimer();
+    }
+}
+
+function startTimer() {
+    var sec = 0.1;
+    intervalID = setInterval(function () {
+        var txt = "Kulunud aeg: ";
+        document.getElementById("timer").innerHTML = txt + (sec).toFixed(1) + " s";
+        sec += 0.1;
+        //if taking too much time, then stop timer
+        if(sec.toFixed(1) == 100) {
+            stopTimer();
+        }
+    }, 100);
+}
+function resetTimer() {
+    clearInterval(intervalID);
+    intervalID = "";
+    document.getElementById("timer").innerHTML = "Kulunud aeg: 0.0 s";
+}
+function stopTimer() {
+    clearInterval(intervalID);
+    intervalID = "";
 }
--- game-1/card.js
+++ game-1/card.js
@@ -1,39 +1,54 @@
 class Card {
     constructor(layerName) {
-        this.layer = document.getElementById(layerName);
+        //this.layer = document.getElementById(layerName);
         window["card_" + layerName] = this;
 
-        this.height = 100;
-        this.width = 100;
-        this.text = "";
+        var height = 100;
+        var width = 100;
+        
         this.layerName = layerName;
 
+        this.text = "";
         this.visible = false;
+        this.found = false;
 
         var txt = '<div id="card_' + this.layerName + '" style="';
-        txt += 'border: 1px solid green; ';
-        txt += 'height: ' + this.height + 'px; width: ' + this.width + 'px; ';
+        txt += 'border: 2px solid black; ';
+        txt += 'height: ' + height + 'px; width: ' + width + 'px; ';
         txt += 'margin: 5px; ';
         txt += 'text-align: center; display:flex; align-items: center; justify-content: center; ';
         txt += 'user-select: none; ';
         txt += '" ';
-        txt += 'onclick="card_' + layerName + '.changeText()"';
-        txt += '><div id="card_' + this.layerName + '_text" style="visibility: ' + ((this.visible) ? 'visible' : 'hidden') + ';">' + this.text + '</div></div>';
+        txt += 'onclick="compareCards(card_' + layerName + ')"';
+        txt += '><div id="card_' + this.layerName + '_text" style="font-weight: bold; visibility: ' + ((this.visible) ? 'visible' : 'hidden') + ';"></div></div>';
         document.getElementById("kast").innerHTML += txt;
     }
 
     insertText(text) {
         this.text = text;
-        document.getElementById("card_" + this.layerName + "_text").innerHTML = text;
     }
 
-    changeText() {
+    changeVisibility() {
+        if (this.found) {
+            return;
+        }
+
         if (this.visible) {
             document.getElementById("card_" + this.layerName + "_text").style.visibility = "hidden";
+            document.getElementById("card_" + this.layerName + "_text").innerHTML = "";
             this.visible = false;
+            document.getElementById("card_" + this.layerName).style.borderColor = "green";
         } else {
             document.getElementById("card_" + this.layerName + "_text").style.visibility = "visible";
+            document.getElementById("card_" + this.layerName + "_text").innerHTML = this.text;
             this.visible = true;
+            document.getElementById("card_" + this.layerName).style.borderColor = "blue";
         }
+    }
+
+    setFound() {
+        this.found = true;
+        document.getElementById("card_" + this.layerName).style.borderColor = "gray";
+        document.getElementById("card_" + this.layerName + "_text").style.color = "gray";
     }
 }
