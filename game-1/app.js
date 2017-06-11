a56f82ff82089f8a6ba2c64a0fead4572c4af39f
Kaido Kukk <kaido.kukk@gmail.com>
2017-06-11 23:54:25 +0300

    Alustasin "omaloomingu" osa võimalusega

--- game-1/app.js
+++ game-1/app.js
@@ -3,11 +3,10 @@ var activeCards = [];
 var intervalID = "";
 var hiddenCardCount = 0;
 var cardTexts = "";
+var customGameElements = 3;
 
 function onPageLoad() {
     showLevelSelection();
-    //document.getElementById("game_header").style.display = "none";
-    //startGame();
 }
 
 function showLevelSelection() {
@@ -27,8 +26,57 @@ function startHardGame() {
     startGame();
 }
 
+//TODO
 function startCustomGame() {
+    let custom_game_content = document.createElement("div");
+    custom_game_content.style.display = "flex";
+    custom_game_content.style.flexDirection = "column";
+    custom_game_content.style.alignItems = "center"; //flex-start
+    custom_game_content.style.width = "100%";
 
+    let label = document.createElement("label");
+    label.textContent = "Sisesta tekstid:";
+    custom_game_content.appendChild(label);
+
+    let text = document.createElement("input");
+    text.setAttribute("type", "text");
+    text.setAttribute("name", "text_" + 1);
+    text.setAttribute("placeholder", "Sisesta tekst");
+    custom_game_content.appendChild(text);
+
+    let newText = text.cloneNode(true);
+    newText.setAttribute("name", "asd");
+    custom_game_content.appendChild(newText);
+
+    let addNewText = document.createElement("input");
+    addNewText.setAttribute("type", "button");
+    addNewText.setAttribute("value", "Lisa uus tekstiväli");
+    custom_game_content.appendChild(addNewText);
+
+    custom_game_content.appendChild(document.createElement("br"));
+
+    let saveSelection = document.createElement("input");
+    saveSelection.setAttribute("type", "button");
+    saveSelection.setAttribute("value", "Salvesta tekstid ja koosta mäng");
+    custom_game_content.appendChild(saveSelection);
+
+    document.getElementById("game_content").appendChild(custom_game_content);
+
+
+
+    /*<label>Sisesta tekstid:</label><br>
+	<input type="text" name="text_1" placeholder="txt1"><br>
+	<input type="text" name="text_2" placeholder="txt2"><br>
+	<input type="button" value="X">
+    document.getElementById("game_content").innerHTML = "";*/
+}
+
+//TODO
+function createNewCustomGameTextNode() {
+    /*let newText = text.cloneNode(true);
+    newText.setAttribute("name", "asd");
+
+    document.getElementById("custom_game_content").insertBefore(newText,document.getElementById("custom_game_content"));*/
 }
 
 function startGame() {
@@ -56,8 +104,6 @@ function clearGameBoard() {
     hiddenCardCount = 0;
     activeCards = [];
     document.getElementById("game_content").innerHTML = "";
-    //var el = document.getElementById("game_content");
-    //while (el.firstChild) el.removeChild(el.firstChild);  //https://jsperf.com/innerhtml-vs-removechild/418
 }
 
 function compareCards(card) {
@@ -105,7 +151,7 @@ function startTimer() {
         var txt = "Kulunud aeg: ";
         document.getElementById("timer").innerHTML = txt + (sec).toFixed(1) + " s";
         sec += 0.1;
-        //if taking too much time, then stop timer
+        //if taking too much time (100 seconds), then stop timer
         if (sec.toFixed(1) == 100 && intervalID != "") {
             stopTimer();
             document.getElementById("timer").innerHTML = "Kulunud aeg: aeg sai otsa!";
